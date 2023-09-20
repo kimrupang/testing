6f49f6eae8eecc6ab7f0bbd323c7404f94afab4a
kimrupang <145536255+kimrupang@users.noreply.github.com>
2023-09-20 16:13:46 +0900

    Update testing.py

--- testing.py
+++ testing.py
@@ -11,6 +11,96 @@ url = 'https://illuminarean.com/'
 
 
 
+def win_handles():
+    driver.switch_to.window(driver.window_handles[-1])
+    time.sleep(1)
+
+
+def scan_errors():
+    driver.get_screenshot_as_file('error.png')
+
+
+def clicks_xpath(address, texts):
+    timecheck = 0
+    while True:
+        try:
+            driver.find_element(By.XPATH, address).click()
+            time.sleep(1)
+            break
+        except Exception:
+            timecheck = timecheck + 1
+            time.sleep(3)
+            if timecheck == rest_time:
+                scan_errors()
+                post_message(texts + "\n클릭이 불가능 합니다.")
+                file_update('error.png')
+                break
+    if timecheck == rest_time:
+        driver.quit()
+        sys.exit()
+
+
+def click_id(address, texts):
+    timecheck = 0
+    while True:
+        try:
+            driver.find_element(By.ID, address).click()
+            time.sleep(1)
+            break
+        except Exception:
+            timecheck += 1
+            time.sleep(3)
+            if timecheck == rest_time:
+                scan_errors()
+                post_message(texts + "\n클릭이 불가능 합니다.")
+                file_update('error.png')
+                break
+    if timecheck == rest_time:
+        driver.quit()
+        sys.exit()
+
+
+def inputkey(address, key, texts):
+    timecheck = 0
+
+    while True:
+        try:
+            driver.find_element(By.XPATH, address).send_keys(key)
+            time.sleep(1)
+            break
+        except Exception:
+            timecheck += 1
+            time.sleep(5)
+            if timecheck == rest_time:
+                scan_errors()
+                post_message(texts + "입력이 불가능합니다.")
+                file_update('error.png')
+                break
+    if timecheck == rest_time:
+        driver.quit()
+        sys.exit()
+
+
+def inputkey_id(address, key, texts):
+    timecheck = 0
+    while True:
+        try:
+            driver.find_element(By.ID, address).send_keys(key)
+            time.sleep(1)
+            break
+        except Exception:
+            timecheck += 1
+            time.sleep(5)
+            if timecheck == rest_time:
+                scan_errors()
+                post_message(texts + "입력이 불가능합니다.")
+                file_update('error.png')
+                break
+    if timecheck == rest_time:
+        driver.quit()
+        sys.exit()
+
+
 
 
 def start(url):
@@ -24,7 +114,6 @@ def start(url):
     #options.add_argument("disable-gpu")
     options.add_argument("lang=ko_KR")
     options.add_experimental_option("detach", True)
-    #service = ChromeService(executable_path=ChromeDriverManager(driver_version="114.0.5735.90").install())
     service = ChromeService(executable_path=ChromeDriverManager().install())
 
     try:
